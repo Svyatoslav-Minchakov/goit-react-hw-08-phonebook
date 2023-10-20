3288cbb77ec88e1524e9590101163a43a8b395e7
Святослав Минчаков <sminchakov@gmail.com>
2023-10-20 23:16:17 +0300

    bagfix

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -64,8 +64,7 @@ export const ContactForm = () => {
           type="text"
           name="name"
           value={name}
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          title="Name may contain only letters"
           required
           onChange={handleInputChange}
         />
@@ -77,8 +76,7 @@ export const ContactForm = () => {
           type="tel"
           name="number"
           value={number}
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          title="Phone may contain only digits"
           required
           onChange={handleInputChange}
         />
